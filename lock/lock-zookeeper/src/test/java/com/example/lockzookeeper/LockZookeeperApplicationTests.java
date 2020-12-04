c84f128d8f8439bfa1de5b30f861ba8819b703f4
长歌行 <wxpro@vip.qq.com>
2020-12-04 18:42:01 +0800

    add:添加zk信息，待完善

--- lock/lock-zookeeper/src/test/java/com/example/lockzookeeper/LockZookeeperApplicationTests.java
+++ lock/lock-zookeeper/src/test/java/com/example/lockzookeeper/LockZookeeperApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.lockzookeeper;
 
+import com.example.lockzookeeper.util.ZKlock;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class LockZookeeperApplicationTests {
+
+    @Autowired
+    ZKlock zKlock;
 
     @Test
     void contextLoads() {
+        zKlock.lock();
+        log.info("上锁了");
+        zKlock.unlock();
+        log.info("解锁了");
     }
 
 }
